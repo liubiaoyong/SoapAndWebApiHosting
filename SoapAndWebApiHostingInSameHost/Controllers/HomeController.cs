7d592c3c3c558e779e8eaecdbe8849e1a2e678df
Sanjay Singh <itssanjay@gmail.com>
2018-02-22 17:24:52 -0500

    initial checkin

--- SoapAndWebApiHostingInSameHost/Controllers/HomeController.cs
+++ SoapAndWebApiHostingInSameHost/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace SoapAndWebApiHostingInSameHost.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : Controller 
     {
         public ActionResult Index()
         {
